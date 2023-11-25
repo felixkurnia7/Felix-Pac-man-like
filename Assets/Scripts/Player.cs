7356eb7a98cd1931c051a491c559fea561fc74be
Felix Kurnia Salim <felixkurnia7@gmail.com>
2023-11-25 20:44:11 +0700

    Player, Camera, Level

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Transform _camera;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        HideAndLockCursor();
+    }
+
+    private void HideAndLockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 horizontalDirection = horizontal * _camera.right;
+        Vector3 verticalDirection = vertical * _camera.forward;
+        horizontalDirection.y = 0;
+        verticalDirection.y = 0;
+
+        Vector3 movementDirection = horizontalDirection + verticalDirection;
+
+        _rigidbody.velocity = movementDirection * _speed * Time.fixedDeltaTime;
+
+    }
+}
