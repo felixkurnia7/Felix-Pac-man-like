30f476dc3cd2e52f3bbfb65588045b4043b6c287
Felix Kurnia Salim <felixkurnia7@gmail.com>
2023-12-14 10:40:09 +0700

    scene management

--- Assets/Pickable/PickableManager.cs
+++ Assets/Pickable/PickableManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PickableManager : MonoBehaviour
 {
@@ -49,6 +50,7 @@ public class PickableManager : MonoBehaviour
         if (_pickableList.Count <= 0)
         {
             Debug.Log("Win");
+            SceneManager.LoadScene("WinScreen");
         }
     }
 }
